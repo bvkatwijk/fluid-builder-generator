a46cc26bd54cb39535f972a6d3818b21d5e24b8e
bvkatwijk <bvkatwijk@gmail.com>
2017-08-05 15:20:05 +0200

    Add microservice setup

---
+++ src/main/scala/org/bvkatwijk/fbg/Application.scala
@@ -0,0 +1,16 @@
+package org.bvkatwijk.fbg
+
+import org.bvkatwijk.fbg.micro.service.MicroService
+
+object Application {
+
+  def main(args: Array[String]): Unit = {
+    MicroService.builder(getClass)
+      .applicationName("Fluid Builder Generator")
+      .homePageFolder("src/main/resources")
+      .homePageFileName("index.html")
+      .build()
+      .start();
+  }
+
+}
