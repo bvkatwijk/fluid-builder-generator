707679c4d12842a6b622c2d50969a8aa30429577
bvkatwijk <bvkatwijk@gmail.com>
2017-08-06 13:22:06 +0200

    Add base ScalaTest for JUnit

---
+++ src/test/scala/org/bvkatwijk/fbg/AbstractScalaTest.scala
@@ -0,0 +1,11 @@
+package org.bvkatwijk.fbg
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+abstract class AbstractScalaTest extends FlatSpec with Matchers {
+  
+}
